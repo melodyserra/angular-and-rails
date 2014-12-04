dede2891af43e7aefffd32ef854c63ae9a4fc8ab
wdi-sf-fall <markus.guehrs@generalassemb.ly>
2014-12-04 09:08:20 -0800

    finished

--- app/assets/javascripts/raffler.js
+++ app/assets/javascripts/raffler.js
@@ -1,46 +1,46 @@
 var app = angular.module("raffler", ["rails"]);
 
 app.factory('Player',
-  function (railsResourceFactory) {
-    var resource = railsResourceFactory({
-      url: '/players',
-      name: 'player'});
-    return resource;
-});
+	function (railsResourceFactory) {
+		var resource = railsResourceFactory({
+			url: '/players',
+			name: 'player'});
+		return resource;
+	});
 
 app.controller('RaffleController', [ 
 	"$scope",
 	"Player",
 	function($scope, Player) {
 
-    Player.query().then(function(result) {
-    	$scope.players = result;
-  	})
- 	
- 		$scope.addPlayer = function() {
+		Player.query().then(function(result) {
+			$scope.players = result;
+		})
+		
+		$scope.addPlayer = function() {
 			var newPlayer = new Player({
-        name: $scope.newName
-    	})
-      newPlayer.create().then(function(newlyCreatedPlayer){
-      	$scope.players.push(newlyCreatedPlayer);
-      	$scope.newName = "";
-      });
-    };
+				name: $scope.newName
+			})
+			newPlayer.create().then(function(newlyCreatedPlayer){
+				$scope.players.push(newlyCreatedPlayer);
+				$scope.newName = "";
+			});
+		};
 
 		$scope.drawWinner = function() {
-      var pool = [];
-      angular.forEach($scope.players, function(player) {
-        if (!player.winner) {
-          return pool.push(player);
-        }
-      });
-      if (pool.length > 0) {
-        var player = pool[Math.floor(Math.random() * pool.length)];
-        player.winner = true;
-        player.update();
-        return $scope.lastWinner = player;
-      }
-    };
+			var pool = [];
+			angular.forEach($scope.players, function(player) {
+				if (!player.winner) {
+					return pool.push(player);
+				}
+			});
+			if (pool.length > 0) {
+				var player = pool[Math.floor(Math.random() * pool.length)];
+				player.winner = true;
+				player.update();
+				return $scope.lastWinner = player;
+			}
+		};
 
- 	}]
-);
+	}]
+	);
